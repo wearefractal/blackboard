0917505adba2366e6b752ef2e27d1410c9ebab98
Contra <contra@maricopa.edu>
2013-10-31 04:10:58 -0700

    more stuff with lessons

---
+++ examples/helloworld.md
@@ -0,0 +1,14 @@
+# Hello World
+
+This is a test lesson
+
+1. console.log hello world
+2. enjoy
+
+```javascript
+var a = 123;
+var b = "Hello!";
+
+testFunction("what what");
+console.log(b.toLowerCase());
+```
--- examples/sample.js
+++ examples/sample.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var path = require('path');
 
 var helpFile = fs.readFileSync(path.join(__dirname, 'help.md'));
+var lessonFile = fs.readFileSync(path.join(__dirname, 'helloworld.md'));
 
 var shop = new blackboard.Class({
   name: "Workshop Test",
@@ -10,11 +11,25 @@ var shop = new blackboard.Class({
 });
 
 var helloWorld = new blackboard.Lesson({
-  name: "Hello World!"
+  name: "Hello World!",
+  instructions: lessonFile,
+  setup: function(box, cb){
+
+  },
+  validate: function(box, cb){
+    
+  }
 });
 
 var babySteps = new blackboard.Lesson({
-  name: "Baby Steps"
+  name: "Baby Steps",
+  instructions: lessonFile,
+  setup: function(cb){
+
+  },
+  validate: function(cb){
+
+  }
 });
 
 shop.add(helloWorld);
